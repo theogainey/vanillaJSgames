904e8f8b3a8c183c2ea911c4098a35f9357e506d
oman1219 <32309792+oman1219@users.noreply.github.com>
2021-07-23 19:42:50 -0400

    undo move

--- freecell/freecell.js
+++ freecell/freecell.js
@@ -2,13 +2,16 @@ var canvas;
 var ctx;
 var game;
 var newGameButton;
-var canvasElements=[];
-var selectedCard;
-var moveStack=[];
 function draw(){
   canvas = document.getElementById('myCanvas');
   canvas.addEventListener('click',(e) => {
-    canvasElements.forEach((item) => {
+    game.cascades.forEach((item) => {
+      item.clickHandler(e.offsetX, e.offsetY,);
+    });
+    game.foundations.forEach((item) => {
+      item.clickHandler(e.offsetX, e.offsetY,);
+    });
+    game.freecells.forEach((item) => {
       item.clickHandler(e.offsetX, e.offsetY,);
     });
   });
@@ -17,26 +20,99 @@ function draw(){
   newGameButton.addEventListener('click',(e) => {
     handleNewGameButton();
   });
+  undoButton = document.getElementById('undoButton');
+  undoButton.addEventListener('click',(e) => {
+    handleUndoMove();
+  });
+
   ctx.fillStyle = 'rgb(37, 128, 25)';
   ctx.fillRect(0, 0, 695, 406);
   game = new FreeCellGame();
+  moveStack = new MoveStack();
 }
 function handleNewGameButton(){
   ctx.fillStyle = 'rgb(37, 128, 25)';
   ctx.fillRect(0, 0, 695, 406);
-  canvasElements=[];
-  selectedCard=null;
-
   game = new FreeCellGame();
 }
+function handleUndoMove(){
+  if (!(game.selectedCard)) {
+    var lastCardMoved = game.moveStack.pop();
+    if (lastCardMoved) {
+    var currentLocation = lastCardMoved.currentLocation;
+     var previousLocation = lastCardMoved.previousLocation;
+    //remove
+    var cardBeingMoved;
+    if (currentLocation<8) {
+      cardBeingMoved = game.cascades[currentLocation].pop();
+      game.cascades[currentLocation].reDraw();
+
+    }
+    else if (currentLocation<12) {
+      cardBeingMoved=  game.freecells[currentLocation-8].pop();
+    }
+    else if(currentLocation<16){
+      cardBeingMoved=  game.foundations[currentLocation-12].pop();
+    }
+
+    //now place
+    if (previousLocation<8) {
+      game.cascades[previousLocation].push(cardBeingMoved, true);
+      game.cascades[previousLocation].reDraw();
+
+    }
+    //if last location a freecell
+   else if (previousLocation<12) {
+      game.freecells[previousLocation-8].push(cardBeingMoved);
+    }
+  }
+  }
+}
+class MoveStack {
+
+  //I think I need to fix it here. store move data and not a card
+  moves= {};
+
+  push(currentLocation, previousLocation){
+    if (!(this.stackPointer)) {
+      var newKey =(previousLocation + currentLocation)+  Math.floor(Math.random() * 52);
+      this.moves[newKey]={
+        currentLocation: currentLocation,
+        previousLocation: previousLocation,
+        ptrPrevious:null,
+      };
+      this.stackPointer = newKey
+    }
+    else {
+      var newKey =(previousLocation + currentLocation)+  Math.floor(Math.random() * 52);
+      this.moves[newKey]={
+        currentLocation: currentLocation,
+        previousLocation: previousLocation,
+        ptrPrevious:this.stackPointer,
+      };
+      this.stackPointer = newKey
+    }
+  }
+  pop(){
+
+    if(this.stackPointer){
+      var move = this.moves[this.stackPointer];
+      delete this.moves[this.stackPointer];
+      this.stackPointer=move.ptrPrevious;
+
+    return move;
+   }
+  }
+}
+
 class FreeSpace{
   card=null;
   isOccupied=false;
-  constructor(x, y){
+  constructor(id, x, y){
+    this.id=id;
     this.x=x;
     this.y=y;
     this.drawSpace();
-    canvasElements.push(this);
   }
   drawSpace(){
     ctx.fillStyle ='white';
@@ -49,32 +125,42 @@ class FreeSpace{
   clickHandler(x, y){
     if( x> this.x && x<(this.x+75)){
       if (y> this.y && y<(this.y+100)) {
-        if (selectedCard && this.isOccupied===false) {
-          this.card= selectedCard;
-          this.card.setNewXY(this.x, this.y);
-          this.isOccupied = true;
-          selectedCard=null;
+        if (game.selectedCard && this.isOccupied===false) {
+          this.push(game.selectedCard);
+          game.moveStack.push(this.card.currentLocation, this.card.previousLocation);
         }
-        else if ((!selectedCard) &&this.isOccupied===true) {
-          selectedCard = this.card;
-          this.card=null;
-          this.isOccupied = false;
-          this.drawSpace();
+        else if ((!game.selectedCard) &&this.isOccupied===true) {
+          game.selectedCard=this.pop();
         }
       }
     }
   }
-
+  push(card){
+    this.card= card;
+    this.card.currentLocation=this.id;
+    this.card.setNewXY(this.x, this.y);
+    this.isOccupied = true;
+    game.selectedCard=null;
+  }
+  pop(){
+    this.card.previousLocation=this.id;
+    let tempCard = this.card;
+    this.card=null;
+    this.isOccupied = false;
+    this.drawSpace();
+    return tempCard
+  }
 }
 class Foundation{
-  card=null;
+  cards={};
+  stackPointer=null;
   nextValue=1;
-  constructor(x, y, suit){
+  constructor(id, x, y, suit){
+    this.id=id;
     this.x=x;
     this.y=y;
     this.suit=suit;
     this.drawSpace();
-    canvasElements.push(this);
   }
   drawSpace(){
     ctx.fillStyle ='white';
@@ -87,19 +173,47 @@ class Foundation{
   clickHandler(x, y){
     if( x> this.x && x<(this.x+75)){
       if (y> this.y && y<(this.y+100)) {
-        if (selectedCard) {
-          if (selectedCard.suit===this.suit&&selectedCard.value===this.nextValue) {
-            this.card= selectedCard.setNewXY(this.x, this.y);
-            this.nextValue++;
-            selectedCard=null;
+        if (game.selectedCard) {
+          if (game.selectedCard.suit===this.suit&&game.selectedCard.value===this.nextValue) {
+            let tempCard = game.selectedCard;
+            tempCard.currentLocation=this.id;
+            this.push(tempCard);
+            game.moveStack.push(tempCard.currentLocation, tempCard.previousLocation);
+            game.selectedCard=null;
           }
         }
       }
     }
   }
+  push(card){
+    var newKey = card.id;
+    this.cards[newKey]={
+      cardData: card,
+      ptrPrevious: this.stackPointer,
+    };
+    this.stackPointer= newKey;
+    card.setNewXY(this.x, this.y);
+    this.nextValue++;
+  }
+  pop(){
+    if (this.stackPointer) {
+      this.nextValue--;
+      let returnCard = this.cards[this.stackPointer].cardData;
+      let tempPointer = this.stackPointer;
+      this.stackPointer =  this.cards[this.stackPointer].ptrPrevious;
+      if (this.stackPointer) {
+        this.cards[this.stackPointer].cardData.drawCard();
+      }
+      else {
+        this.drawSpace();
+      }
+      delete this.cards[tempPointer];
+      return returnCard
+    }
+  }
 }
 
-class CardDisplay{
+class Card{
   constructor(card, x, y){
     this.x=x;
     this.y=y;
@@ -168,12 +282,14 @@ class CardDisplay{
 class FreeCellGame{
   constructor(){
     this.cascades = this.deal();
-    this.freeceels= [new FreeSpace(2, 2), new FreeSpace( 79, 2), new FreeSpace( 156, 2), new FreeSpace( 233, 2)];
-    this.foundations= [new Foundation( 387, 2,"Hearts"), new Foundation( 464, 2, "Clubs"), new Foundation( 541, 2,"Diamonds"), new Foundation( 618, 2, "Spades")];
+    this.freecells= [new FreeSpace(8, 2, 2), new FreeSpace(9, 79, 2), new FreeSpace(10, 156, 2), new FreeSpace(11, 233, 2)];
+    this.foundations= [new Foundation(12, 387, 2,"Hearts"), new Foundation(13, 464, 2, "Clubs"), new Foundation(14, 541, 2,"Diamonds"), new Foundation(15, 618, 2, "Spades")];
+    this.moveStack= new MoveStack();
+    this.selectedCard = null;
   }
   deal(){
     var shuffledDeck = getShuffledCardDeck();
-    var cascades = [new Cascade(2,112),new Cascade(79,112),new Cascade(156,112),new Cascade(233,112),new Cascade(387,112),new Cascade(464,112),new Cascade(541,112),new Cascade(618,112)];
+    var cascades = [new Cascade(0, 2,112),new Cascade(1, 79,112),new Cascade(2, 156,112),new Cascade(3, 233,112),new Cascade(4, 387,112),new Cascade(5, 464,112),new Cascade(6, 541,112),new Cascade(7, 618,112)];
     var cascadeIndex=0;
     shuffledDeck.forEach((item) => {
       let tempcascade = cascades[cascadeIndex];
@@ -192,34 +308,48 @@ class FreeCellGame{
 
 class Cascade {
   cards= {};
-  constructor(x,y){
+  constructor(id, x,y){
+    this.id= id;
     this.x=x;
     this.y=y;
-    canvasElements.push(this);
   }
   clickHandler(x, y){
     if( x> this.x && x<(this.x+75)){
       if (y> this.y && y<(this.y+(this.count()*15)+100)) {
-        if (!selectedCard) {
-          selectedCard = this.pop();
+        if (!game.selectedCard) {
+          game.selectedCard = this.pop();
+          this.perviousTail= game.selectedCard;
           this.reDraw();
         }
-        else if (selectedCard ) {
-          if (selectedCard.red===true) {
-            if (this.cards[this.tailPtr].cardData.red===false && ((selectedCard.value + 1) ===this.cards[this.tailPtr].cardData.value)) {
-              this.push(selectedCard)
-              selectedCard=null;
+        else if (game.selectedCard.currentLocation===this.id) {
+          this.push(game.selectedCard);
+          game.selectedCard=null;
+          this.perviousTail=null;
+        }
+        else if (game.selectedCard ) {
+          if (game.selectedCard.red===true) {
+            if (this.cards[this.tailPtr]){
+              if (this.cards[this.tailPtr].cardData.red===false && ((game.selectedCard.value + 1) ===this.cards[this.tailPtr].cardData.value)){
+                this.push(game.selectedCard)
+                game.selectedCard=null;
+              }
+            }
+            else {
+              this.push(game.selectedCard)
+              game.selectedCard=null;
             }
           }
-          else if (selectedCard.red===false) {
-            if (this.cards[this.tailPtr].cardData.red===true && ((selectedCard.value + 1) ===this.cards[this.tailPtr].cardData.value)) {
-              this.push(selectedCard)
-              selectedCard=null;
+          else if (game.selectedCard.red===false) {
+            if (this.cards[this.tailPtr]) {
+              if (this.cards[this.tailPtr].cardData.red===true && ((game.selectedCard.value + 1) ===this.cards[this.tailPtr].cardData.value)) {
+                this.push(game.selectedCard)
+                game.selectedCard=null;
+              }
             }
-          }
-          else if (this.count()===0) {
-            this.push(selectedCard)
-            selectedCard=null;
+            else {
+              this.push(game.selectedCard)
+              game.selectedCard=null;
+            }
           }
         }
       }
@@ -236,14 +366,25 @@ class Cascade {
       pointerNext = tempCard.ptrNext;
     }
   }
-  push(card){
-
+  push(card, isUndo){
+    if (!(card instanceof Card) ) {
+      card = new Card(card, this.x, this.y+(this.count()*15));
+      card.currentLocation = this.id;
+      card.previousLocation = this.id;
+    }
+    else {
+      card.setNewXY(this.x, this.y+(this.count()*15));
+      card.currentLocation = this.id;
+      if (!isUndo) {
+        game.moveStack.push(card.currentLocation, card.previousLocation);
+      }
+    }
     if(this.count()===0){
       var newKey = card.id;
       this.headPtr = newKey;
       this.tailPtr= newKey;
       this.cards[newKey]={
-        cardData: new CardDisplay(card, this.x, this.y+(this.count()*15)),
+        cardData: card,
         ptrNext: null,
         ptrPrevious: null,
       };
@@ -251,7 +392,7 @@ class Cascade {
     else{
       var newKey = card.id;
       this.cards[newKey]={
-        cardData: new CardDisplay(card, this.x, this.y+(this.count()*15)),
+        cardData: card,
         ptrNext: null,
         ptrPrevious: this.tailPtr,
       };
@@ -270,7 +411,9 @@ class Cascade {
       }
       delete this.cards[this.tailPtr];
       this.tailPtr=tail.ptrPrevious;
-    return tail.cardData;
+      var returnCard = tail.cardData;
+      returnCard.previousLocation=this.id;
+    return returnCard;
    }
   }
   count() {
